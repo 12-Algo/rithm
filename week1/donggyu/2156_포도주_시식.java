25bf200be78ede4141542054421cd86f7fc62b92
donggyu <use02012@gmail.com>
2025-02-25 17:27:13 +0900

    0225 donggyu

--- week1/donggyu/2156_포도주_시식.java
+++ week1/donggyu/2156_포도주_시식.java
@@ -5,8 +5,11 @@ import java.util.*;
 
 public class BOJ_2156 {
 
+//	포도주 잔 개수
     static int bottles;
+//  포도주 양
     static Integer[] amounts;
+//  dp 테이블
     static Integer[] dp;
 
     public static void main(String[] args) throws IOException {
@@ -20,12 +23,15 @@ public class BOJ_2156 {
         }
 
         dp = new Integer[bottles+1];
-
+        
         dp[0] = 0;
         dp[1] = amounts[1];
+//      포도주 잔 개수가 1개 이하일 경우 
         if(bottles>1)
             dp[2] = amounts[1]+amounts[2];
-
+        
+//      연속으로 3잔을 연속해서 먹을 수 없기 때문에 i번째 포도주 잔의 최댓값
+//      = Max(i-2번째까지의 최대값+i번째 포도주 용량 /i-1번째까지의 최대값(i번째 채택X) / i-3번째까지의 최대값+i-1번째 포도주 용량+i번째 포도주 용량)
         for(int i = 3; i < dp.length; i++) {
             dp[i] = Math.max(Math.max(dp[i-2]+amounts[i], dp[i-1]), dp[i-3]+amounts[i-1]+amounts[i]);
         }
